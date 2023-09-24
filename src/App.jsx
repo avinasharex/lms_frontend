c230e7ca390dd1d3ddd241b236d901b2422e1485
avinasharex <avinash462470@gmail.com>
2023-09-24 15:37:08 +0530

    configure auto import sorting for eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { Route, Routes } from "react-router-dom"
 
 function App() {
 
   return (
     <>
-      lms
+      <Routes>
+        <Route></Route>
+      </Routes>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import './index.css';
+
 import ReactDOM from 'react-dom/client'
+import BrowseRouter from 'react-router-dom'
+
 import App from './App.jsx'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+    <BrowseRouter>
     <App />
+    </BrowseRouter>
 )
