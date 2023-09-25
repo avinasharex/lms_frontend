4cf7e73830979172d2daadc0168736c2d9232931
avinasharex <avinash462470@gmail.com>
2023-09-25 22:41:52 +0530

    Added login signup button to sidebar

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -1,9 +1,17 @@
 import {AiFillCloseCircle} from 'react-icons/ai'
 import {FiMenu} from 'react-icons/fi'
-import {Link} from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import {Link, useNavigate} from 'react-router-dom'
 
 import Footer from '../components/Footer'
 function HomeLayout({children}) {
+    //    const dispatch = useDispatch()
+       const navigate = useNavigate()
+
+       // For checking if user is logged in
+       const isLoggedIn = useSelector(state=>state?.auth?.isLoggedIn)
+       // For displaying the option acc to role
+       const role = useSelector(state=> state?.auth?.role)
 
     function changeWidth(){
         const drawerSide = document.getElementsByClassName("drawer-side")
@@ -14,6 +22,15 @@ function HomeLayout({children}) {
         element[0].checked = false
         // changeWidth()
     }
+    function handleLogout(e){
+        e.preventDefault()
+
+        // const res = await dispatch(logout())
+        // if(res?.payload?.success)
+
+        navigate('/')
+    }
+    
   return (
     <>
     <div className="min-h-[90vh]" data-theme="dark">
@@ -41,6 +58,11 @@ function HomeLayout({children}) {
                         <li>
                             <Link to='/'>Home</Link>
                         </li>
+                        {isLoggedIn && role === 'ADMIN' &&(
+                            <Link to={'/admin/dashboard'}>
+                                Admin Dashboard
+                            </Link>
+                        )}
                         <li>
                             <Link to='/courses'>All Courses</Link>
                         </li>
@@ -50,6 +72,26 @@ function HomeLayout({children}) {
                         <li>
                             <Link to='/about'>About Us</Link>
                         </li>
+                        {!isLoggedIn && (
+                            <div className='w-full flex items-center justify-center'>
+                                    <button className='btn-primary px-4 py-1 font-semibold w-full rounded-md'>
+                                        <Link to={'/login'}>Login</Link>
+                                    </button>
+                                    <button className='btn-secondary px-4 py-1 font-semibold w-full rounded-md'>
+                                        <Link to={'/signup'}>Signup</Link>
+                                    </button>
+                            </div>
+                        )}
+                        {!isLoggedIn && (
+                            <div className='w-full flex items-center justify-center'>
+                                    <button className='btn-primary px-4 py-1 font-semibold w-full rounded-md'>
+                                        <Link to={'/user/profile'}>Profile</Link>
+                                    </button>
+                                    <button className='btn-secondary px-4 py-1 font-semibold w-full rounded-md'>
+                                        <Link onClick={handleLogout}>Logout</Link>
+                                    </button>
+                            </div>
+                        )}
                     </ul>
                 </div>
         </div>
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -4,6 +4,7 @@ import homeImage from '../Assests/Header.webp'
 import HomeLayout from "../Layouts/HomeLayout"
 
 function HomePage() {
+ 
   return (
     <HomeLayout>
         <div className="pt-10 text-white flex items-center justify-center gap-10 mx-16 h-[90vh]">
