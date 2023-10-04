5764ef965fb2fb0de1777109f978f0bed4ec21d4
avinasharex <avinash462470@gmail.com>
2023-10-04 12:20:57 +0530

    Added require auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 
 import { Route, Routes } from "react-router-dom"
 
+import RequireAuth from "./Auth/RequireAuth"
 import AboutPage from "./Pages/AboutPage"
 import Contact from "./Pages/Contact"
 import CourseDescription from "./Pages/Course/CourseDescription"
 import CourseList from "./Pages/Course/CourseList"
+import CreateCourse from "./Pages/Course/CreateCourse"
 import Denied from "./Pages/Denied"
 import HomePage from "./Pages/HomePage"
 import Login from "./Pages/Login"
@@ -23,6 +25,10 @@ function App() {
     <Route path="/contact" element={<Contact></Contact>}></Route>
     <Route path="/denied" element={<Denied></Denied>}></Route>
     <Route path="/courses/description" element={<CourseDescription></CourseDescription>}></Route>
+
+    <Route element={<RequireAuth allowedRole={["ADMIN"]} />}>
+      <Route path="/course/create" element={<CreateCourse></CreateCourse>}></Route>
+      </Route>
 
 
     <Route path="/signup" element={<Signup></Signup>}></Route>
