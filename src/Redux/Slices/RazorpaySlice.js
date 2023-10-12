d5ad30a6750a25b253acf69b894a42dc036b80fa
avinasharex <avinash462470@gmail.com>
2023-10-12 10:24:53 +0530

    Added razopay slice

---
+++ src/Redux/Slices/RazorpaySlice.js
@@ -0,0 +1,105 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import toast from "react-hot-toast"
+
+import axiosInstance from "../../Helpers/AxiosInstance"
+
+const initialState = {
+    key : '',
+    subscription_id : '',
+    isPaymentVerified : false,
+    allPayments : {},
+    finalMonths : {},
+    monthlySalesRecord : []
+}
+
+export const getRazorPayId = createAsyncThunk("razorpay/getid", async()=>{
+    try {
+        const response = await axiosInstance.get(`http://localhost:5000/api/v1/user/courses/razorpay-key`)
+        return response.data
+    } catch (error) {
+        toast.error("Failed to load data")
+    }
+})
+
+export const purchaseCourseBundle = createAsyncThunk("purchaseCourse", async()=>{
+    try {
+        const response = await axiosInstance.post(`http://localhost:5000/api/v1/user/courses/subscribe`)
+        return response.data
+    } catch (error) {
+        toast.error(error?.response?.data?.message)
+    }
+})
+
+export const verifyUserPayment = createAsyncThunk("payment/verify", async(data)=>{
+    try {
+        const response = await axiosInstance.post(`http://localhost:5000/api/v1/user/courses/verify`,{
+            razorpay_payment_id: data.razorpay_payment_id,
+            razorpay_subscription_id: data.razorpay_subscription_id,
+            razorpay_signature: data.razorpay_signature
+        })
+        return response.data
+    } catch (error) {
+        toast.error(error?.response?.data?.message)
+    }
+})
+
+export const getPaymentRecord = createAsyncThunk("payment/records", async()=>{
+    try {
+        const response =  axiosInstance.get(`http://localhost:5000/api/v1/user/courses/payment?count=100`)
+        const successMessage = response?.data?.message
+        toast.promise(successMessage, {
+            loading: 'Getting the payment record',
+            success: successMessage,
+            error: 'Failed to get payment records'
+        })
+        return response.data
+    } catch (error) {
+        toast.error(error?.response?.data?.message)
+    }
+})
+
+export const cancelCourseBundle = createAsyncThunk("payment/cancel", async()=>{
+    try {
+        const response =  axiosInstance.post(`http://localhost:5000/api/v1/user/courses/unsubscribe`)
+        const successMessage = response?.data?.message
+        toast.promise(successMessage, {
+            loading: 'Unsubscribing the bundle',
+            success: successMessage,
+            error: 'Failed to unsubscribe'
+        })
+        return response.data
+    } catch (error) {
+        toast.error('Operation failed')
+    }
+})
+
+const razorpaySlice = createSlice({
+    name: 'razorpay',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+        .addCase(getRazorPayId.fulfilled, (state,action)=>{
+            state.key = action?.payload?.key
+        })
+        .addCase(purchaseCourseBundle.fulfilled,(state,action)=>{
+            state.subscription_id = action?.payload?.subscription_id
+        })
+        .addCase(verifyUserPayment.fulfilled,(state,action)=>{
+            toast.success(action?.payload?.message)
+            state.isPaymentVerified = action?.payload?.success
+        })
+        .addCase(verifyUserPayment.rejected,(state,action)=>{
+            toast.success(action?.payload?.message)
+            state.isPaymentVerified = action?.payload?.success
+        })
+        .addCase(getPaymentRecord.fulfilled, (state,action)=>{
+            state.allPayments = action?.payload?.allPayments
+            state.finalMonths = action?.payload?.finalMonths
+            state.monthlySalesRecord = action?.payload?.monthlySalesRecord
+        })
+
+    }
+})
+
+export default razorpaySlice.reducer
